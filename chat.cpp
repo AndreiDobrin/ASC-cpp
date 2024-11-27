69734dd6211948514e3004985f0038a01f5c1ba6
AndreiDobrin <andrei.dobrin901@gmail.com>
2024-11-27 21:12:59 +0200

    closer to da goal

--- chat.cpp
+++ chat.cpp
@@ -2,38 +2,31 @@
 //add 5
 //add 5 3
 // *no bueno*
-
 #include <iostream>
 using namespace std;
 #include <cstring>
 
 void add(int &n, int v[]){
     int ok=0;
-    int x, y;
+    int nr;
     int pos1=0, pos2=0;
-    int i=0;
+    int k=0;
+    int descr,dim;
+    int i1=0,i2=0;
+        cin>>nr;
+        while(nr>0){
+            cin>>descr;
+            cin>>dim;
 
-        cin>>x;
-        cin>>y;
-        while(ok<y && i<10){
-            if(v[i]==0){
-                ok++;
-                pos2=i+1;
-            
+            for(k=0;k<dim/8+1;k++){
+                v[i2++] = descr;
             }
-            if(v[i]!=0){
-                ok=0;
-                pos1=i+1;
-                pos2=pos1;
-            }
-            i++;
+            nr--;
+            cout<<descr<<": ("<<i1<<','<<i2-1<<")\n";
+            i1=i2;
         }
-        if(ok==y){
-            for(int i=pos1;i<pos2;i++)
-                v[i]=x;
-        }
-        else
-            cout<<"Nu mai exista spatiu ramas, incercati o defragmentare..."<<endl;
+
+        
 }
 
 void get(int &n, int v[]){
@@ -68,37 +61,77 @@ void del(int &n, int v[]){
     } 
 }
 
+void defrag(int &n, int v[]){
+    int i = 0;
+    int pos1,pos2;
+    int aux,ok;
+    int j = 0;
+    while(i<n){
+        ok = 0;
+
+        if(v[i] == 0)
+        {
+
+            for(int k=i;k<n;k++)
+                if(v[k]!=0){
+                    ok = 1;
+                    break;
+                }
+            
+            if(ok==1){
+                pos1 = i;
+                while(v[i]==0)
+                    i++;
+                pos2 = i;
+                j = pos2-pos1;
+                while(j>0){
+                    aux=v[pos1];
+                    v[pos1]=v[pos2];
+                    v[pos2]=aux;
+                    pos1++;
+                    pos2++;
+                    j--;
+                }
+            }
+        }
+        i++;
+    }  
+}
+
 
 int main() {
-    
+    char s[30];
     int v[10],n=10;
     for(int i=0;i<10;i++)
     v[i]=0;
     int x,y;
-    char inst[10];
+    int inst;
     int i;
-    while(1){
-        cin>>inst;
-        if(strcmp(inst,"exit")==0){
-            break;
-        }
-        else if((strcmp(inst,"add")==0))
+    int nr;
+    cin>>nr;
+    cin>>inst;
+    while(nr>0){
+        if(inst==1)
             add(n,v);
         
-        else if(strcmp(inst,"show")==0){
+        else if(inst==5){
             for(int i=0;i<10;i++)
                 cout<<v[i]<<" ";
             cout<<endl;
         }
-        else if(strcmp(inst,"get")==0){
+        else if(inst==2){
             get(n,v);
         }
-        else if(strcmp(inst,"del")==0){
+        else if(inst==3){
             del(n,v);
         }
+        else if(inst==4){
+            defrag(n,v);
+        }
+        nr--;
         
-    for(int i=0;i<10;i++)
-        cout<<v[i]<<" ";
-    cout<<endl;
+    //for(int i=0;i<10;i++)
+     //   cout<<v[i]<<" ";
+    //cout<<endl;
     }
 }
